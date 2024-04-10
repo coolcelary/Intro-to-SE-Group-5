cf0e7f95faa8ee9539baf15f991013fbe56b1b61
Charles <chaseuelt@gmail.com>
2024-04-10 13:22:30 -0500

    added new tests and fixed database issue

--- run_tests.sh
+++ run_tests.sh
@@ -17,12 +17,12 @@ print_red() {
 check_curl() {
 	local url="$1"
 	local response=$(curl -L -X GET -s -o curl.out -w "%{http_code}" "$url")
-	cat ./curl.out
-	echo
 
 	if [ "$response" -eq 200 ]; then
 		print_green "curl to URL $url is successful"
 	else
+		cat ./curl.out
+		echo
 		print_red "curl to URL $url failed"
 		exit 1
 	fi
@@ -34,6 +34,86 @@ terminate() {
 }
 
 trap terminate SIGINT
+
+print_title "Creating Testing Database"
+
+# Refresh the database
+rm -f ./backend/EcommerceDB.db
+touch ./backend/EcommerceDB.db
+
+# Create Database structure
+sqlite3 ./backend/EcommerceDB.db "CREATE TABLE Authentication (
+    UserID INTEGER PRIMARY KEY,
+    Username TEXT NOT NULL,
+    Password TEXT NOT NULL,
+    UserType TEXT NOT NULL,
+    Email TEXT,
+    PhoneNumber TEXT
+);"
+sqlite3 ./backend/EcommerceDB.db "CREATE TABLE Orders (
+    OrderID INTEGER PRIMARY KEY,
+    UserID INTEGER NOT NULL,
+    ProductID INTEGER NOT NULL,
+    Quantity INTEGER,
+    Name TEXT,
+    Address TEXT,
+    Email TEXT,
+    CardNumber TEXT,
+    ExpirationDate TEXT,
+    CardName TEXT,
+    CVV INTEGER
+);"
+sqlite3 ./backend/EcommerceDB.db "CREATE TABLE contact (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    email TEXT NOT NULL,
+    message TEXT NOT NULL
+);"
+sqlite3 ./backend/EcommerceDB.db "CREATE TABLE Cart (
+    UserID INTEGER,
+    ProductID INTEGER,
+    Quantity INTEGER,
+    FOREIGN KEY (UserID) REFERENCES Authentication(UserID),
+    FOREIGN KEY (ProductID) REFERENCES Products(ProductID)
+);"
+sqlite3 ./backend/EcommerceDB.db "CREATE TABLE Reviews (
+    ReviewID INTEGER PRIMARY KEY,
+    ProductID INTEGER,
+    Username TEXT,
+    Rating INTEGER,
+    ReviewText TEXT,
+    FOREIGN KEY (ProductID) REFERENCES Products(ProductID)
+);"
+sqlite3 ./backend/EcommerceDB.db "CREATE TABLE admin (
+    AdminID INTEGER PRIMARY KEY,
+    Username TEXT,
+    Password TEXT
+);"
+sqlite3 ./backend/EcommerceDB.db "CREATE TABLE products (
+    product_id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    price REAL NOT NULL,
+    category TEXT NOT NULL,
+    image_url TEXT,
+    SellerID INTEGER,
+    FOREIGN KEY (SellerID) REFERENCES Sellers(SellerID)
+);"
+
+# Add preliminary data
+sqlite3 ./backend/EcommerceDB.db "INSERT INTO admin (AdminID, Username, Password) VALUES (NULL, 'testing', 'testing');"
+
+print_title "Adding Products..."
+
+sqlite3 ./backend/Products.db "SELECT * FROM products;" | while IFS="|" read -r product_id name price category image_url SellerID; do
+	nm=$(echo $name | sed "s/'//g")
+	pr=$(echo $price | sed "s/'//g")
+	ct=$(echo $category | sed "s/'//g")
+	img=$(echo $image_url | sed "s/'//g")
+	sel=$(echo $SellerID | sed "s/'//g")
+	insert_stmt="INSERT INTO products (product_id, name, price, category, image_url, SellerID) VALUES (NULL, '$nm', '$pr', '$ct', '$img', '$sel')"
+	echo $insert_stmt
+	sqlite3 ./backend/EcommerceDB.db "$insert_stmt"
+done
 
 print_title "Running Python Backend Tests:"
 
@@ -74,7 +154,7 @@ sleep 5
 check_curl "http://localhost:3000/login"
 check_curl "http://localhost:3000/register"
 check_curl "http://localhost:3000/inventory"
-#check_curl "http://localhost:3000/contact"
+check_curl "http://localhost:3000/contact"
 check_curl "http://localhost:3000/about"
 check_curl "http://localhost:3000/checkout"
 check_curl "http://localhost:3000/admin"
